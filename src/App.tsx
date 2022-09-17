8b0265f0ffd53b141b17dbd6ba3149efda7cb1cd
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-17 17:00:55 -0300

    another repo/ pieces on board

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Chessboard from "./components/Chessboard/Chessboard";
+
+function App() {
+  return (
+    <div id="app">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
